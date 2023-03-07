67e3023280dbfe5cf4a1b6c0e28f09ed24924586
Chris Storz <chris.storz@harness.io>
2023-03-07 11:06:55 -0800

    database terraform

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# migration_with_ff
+# Setup
+
+## Create Database
+
+#### Environment variables 
+Prior to running Terraform, create necessary environment variables
+
+```
+export DATABASE_PASSWORD="harnessisbest"
+export SECURITY_GROUP="sg-abc123"
+export SUBNET_GROUP="default-vpc-abc123"
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
+export AWS_SESSION_TOKEN=""
+```
+
+#### Run Terraform
+
+```
+ terraform init
+ 
+ terraform apply -var "database_password=$DATABASE_PASSWORD"  -var "security_group=$SECURITY_GROUP" -var "subnet_group=$SUBNET_GROUP"
+```
+
+### Warning
+AWS Costs Money
---
+++ database.tf
@@ -0,0 +1,35 @@
+variable "database_password" {
+  description = "Password for the database user"
+  type        = string
+}
+
+variable "security_group" {
+  description = "Security group (e.g. sg-0123456789abcdef)"
+  type        = string
+}
+
+variable "subnet_group" {
+  description = "Subnet group (e.g. my-subnet-group)"
+  type        = string
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_db_instance" "postgres" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  engine_version       = "14.6"
+  instance_class       = "db.t3.micro"
+  name                 = "harnessff"
+  username             = "postgres"
+  password             = "${var.database_password}"
+  publicly_accessible = true
+  vpc_security_group_ids = [var.security_group]
+  db_subnet_group_name      = var.subnet_group
+}
+
+output "database_endpoint" {
+  value = aws_db_instance.postgres.endpoint
+}
